b766d22dc1985b5e2efde3feb9892c13af965d59
BenniWi <benjamin.wilking@gmail.com>
2023-04-24 14:18:28 +0000

    added code and chapter for classes and references

---
+++ code/part_2/basics_in_Cpp/pointer_vs_reference.cpp
@@ -0,0 +1,34 @@
+
+
+class MyTestClass
+{
+   public:
+    int test_member = 0;
+};
+
+void function_with_reference(MyTestClass &mtc)
+{
+    mtc.test_member = 1;
+}
+
+void function_with_pointer(MyTestClass *mtc)
+{
+    mtc->test_member = 2;
+}
+
+auto main() -> int
+{
+    int i = 3;
+    int a = 10;
+
+    // A pointer to variable i or "stores the address of i"
+    int *ptr = &i;
+
+    // A reference (or alias) for i.
+    int &ref = i;
+    
+    // reassignment:
+    ptr = &a;  // that's fine
+    // &ref = a;  // It is incorrect as we should declare and initialize references at single step
+    // this means: also re-initialization is not possible
+}
