26460dabe30a7a5d235fc67a08e45adceb7196e9
BenniWi <benjamin.wilking@gmail.com>
2023-05-05 14:18:56 +0000

    another linter fix

--- code/part_2/tdd/test/test_functions.cpp
+++ code/part_2/tdd/test/test_functions.cpp
@@ -3,18 +3,18 @@
 #include "gtest/gtest.h"  // include the gtest functions & macros
 
 // the first test we want to write
-TEST(tdd_tests, AddStandardIntegers)
+TEST(TddTests, AddStandardIntegers)
 {
     EXPECT_EQ(11, add_integers(5, 6));
 }
 
-TEST(tdd_tests, AddZeros)
+TEST(TddTests, AddZeros)
 {
     EXPECT_EQ(0, add_integers(0, 0));
 }
 
 #if 0
-TEST (tdd_tests, overflow) { 
+TEST (TddTests, overflow) { 
     EXPECT_EQ (2147483648, add_integers(2147483647,1));
 }
 #endif
